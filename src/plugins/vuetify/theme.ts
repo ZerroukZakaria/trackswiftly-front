8ff729d3a89617f80e500c4230dc98d070cf8c09
ZerroukZakaria <zerrouk.zakaria2002@gmail.com>
2025-02-28 17:37:17 +0000

    changed default theme color to black

--- src/plugins/vuetify/theme.ts
+++ src/plugins/vuetify/theme.ts
@@ -1,6 +1,6 @@
 import type { ThemeDefinition } from 'vuetify'
 
-export const staticPrimaryColor = '#7367F0'
+export const staticPrimaryColor = '#000000'
 
 export const themes: Record<string, ThemeDefinition> = {
   light: {
